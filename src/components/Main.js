9df63f3c73411b44021c88bc2c02ff09b10deeca
sho6ix <shota09060906@gmail.com>
2022-09-02 18:38:46 +0900

    apply grid layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
 
-const Main = () => {
-    return <div>Main</div>
+import MassageList from "./MassageList";
+import MassageInputField from "./MassageInputField";
+
+const useStyles = makeStyles({
+    root: {
+        display: 'grid',
+        height: '100vh',
+        gridTemplateRows: '1fr auto',
+    },
+});
+
+const Main = ({ name }) => {
+    const classes = useStyles();
+    return (
+    <div className={classes.root}>
+        <MassageList />
+
+        <MassageInputField />
+    </div>
+    )
 };
 
 export default Main;

a9ee461536400b2e9c7d9431042525edea99f27a
sho6ix <shota09060906@gmail.com>
2022-09-03 17:46:14 +0900

    apply grid to input component

--- src/components/MassageInputField.js
+++ src/components/MassageInputField.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { Grid, Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     root: {
         gridRow: 2,
+        margin: '26px',
     },
 });
 
 const MassageInputField = () => {
     const classes = useStyles();
-    return <div className={classes.root}>MassageInputField</div>
+    return(
+        <div className={classes.root}>
+            <Grid container>
+                <Grid item xs={1}>
+                    <Avatar />
+                </Grid>
+                <Grid item xs={10}>input</Grid>
+                <Grid item xs={1}>button</Grid>
+            </Grid>
+        </div>
+    ) 
 };
 
 export default MassageInputField;

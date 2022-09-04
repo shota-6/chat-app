302b9718f821520d2c95aea8cdb6dba82823134f
sho6ix <shota09060906@gmail.com>
2022-09-04 18:39:54 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,27 @@
 import firebase from 'firebase';
 
+const {
+REACT_APP_FIREBASE_API_KEY,
+REACT_APP_FIREBASE_AUTH_DOMAIN,
+REACT_APP_FIREBASE_PROJECT_ID,
+REACT_APP_FIREBASE_STORAGE_BUCKET,
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+REACT_APP_FIREBASE_APP_ID,
+} = process.env
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCx9y_xBdmQvC9O52rYeQ3cQ9bSfYg72oE",
-  authDomain: "chat-app-f4254.firebaseapp.com",
-  projectId: "chat-app-f4254",
-  storageBucket: "chat-app-f4254.appspot.com",
-  messagingSenderId: "966355082793",
-  appId: "1:966355082793:web:fe9691691fd52fd4960638",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('messages');
+const messagesRef = database.ref('messages');
 
 export const PushMessage = ({ name ,text}) => {
-    messageRef.push({name, text});
+    messagesRef.push({name, text});
 }

66d71a035fb08ca0c6047bc1b246ace8ff3eb8be
sho6ix <shota09060906@gmail.com>
2022-09-06 16:57:30 +0900

    display messages

--- src/components/MassageList.js
+++ src/components/MassageList.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { List } from "@material-ui/core";
 
 import { messagesRef } from "../firebase";
+import MessageItem from "./MessageItem";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
+    width: '100%',
+    overflow: 'auto',
   },
 });
 
@@ -15,24 +19,30 @@ const MassageList = () => {
 
   useEffect(() => {
     messagesRef
-    .orderByKey()
-    .limitToLast(3)
-    .on("value", (snapshot) => {
-      const messages = snapshot.val();
-      
-      if(messages === null) return;
+      .orderByKey()
+      .limitToLast(15)
+      .on("value", (snapshot) => {
+        const messages = snapshot.val();
 
-      const entries = Object.entries(messages);
-      const newMessages = entries.map((entry) => {
-        const [key, nameAndText] = entry;
+        if (messages === null) return;
 
-        return { key, ...nameAndText };
+        const entries = Object.entries(messages);
+        const newMessages = entries.map((entry) => {
+          const [key, nameAndText] = entry;
+
+          return { key, ...nameAndText };
+        });
+        setMessages(newMessages);
       });
-      setMessages(newMessages);
-    });
   }, []);
 
-  return <div className={classes.root}>MassageList</div>;
+  return (
+    <List className={classes.root}>
+      {messages.map(({key,name,text}) => {
+        return <MessageItem key={key} name={name} text={text}>item</MessageItem>;
+      })}
+    </List>
+  );
 };
 
 export default MassageList;
